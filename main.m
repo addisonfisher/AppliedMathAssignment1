4da618940a7b4a0054ce25ff145f74f0c259ede9
Addison Fisher <afisher@olin.edu>
2025-09-05 16:13:26 -0400

    i let orion touch it

--- main.m
+++ main.m
@@ -1,25 +1,22 @@
-% --- Main Script ---
+function main()
 
-test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) - 0.7 - exp(x/6);
+    %first root
+    x_left = -15;
+    x_right = 10;
+    
+    root1 = bisection_solver(@fun, x_left, x_right);
+    
+    fprintf('The first root found is: %.14f\n', root1);
+    
+    %second root
+    x_left = 25;
+    x_right = 40;
+    root2 = bisection_solver(@fun, x_left, x_right);
+    fprintf('The second root found is: %.14f\n', root2);
 
-
-%how would this be imported into the function like as is shown in the hw?
-function [f, dfdx] = fun(x)
-    f = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) - 0.7 - exp(x/6);
-    dfdx = @(x) (3*x.^2)/100 - (x)/4 + 2 + 3*cos(x/2 + 6)*0.5 - (1/6)*exp(x/6);
 end
 
-
-%first root
-x_left = -15;
-x_right = 10;
-
-root1 = bisection_solver(test_func01, x_left, x_right);
-
-fprintf('The first root found is: %.14f\n', root1);
-
-%second root
-x_left = 25;
-x_right = 40;
-root2 = bisection_solver(test_func01, x_left, x_right);
-fprintf('The second root found is: %.14f\n', root2);
+function [f, dfdx] = fun(x)
+    f =  (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) - 0.7 - exp(x/6);
+    dfdx =  (3*x.^2)/100 - (x)/4 + 2 + 3*cos(x/2 + 6)*0.5 - (1/6)*exp(x/6);
+end
