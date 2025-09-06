991bf5df03e1ab5a98928aeebff82e6a838a5af4
Addison Fisher <afisher@olin.edu>
2025-09-06 15:42:43 -0400

    changes

--- bisection_solver.m
+++ bisection_solver.m
@@ -1,4 +1,4 @@
-function x = bisection_solver(fun, x_left, x_right)
+function [x, guesses_c, guesses_fc] = bisection_solver(fun, x_left, x_right)
     if sign(fun(x_left)) == sign(fun(x_right))
         error('The initial interval does not cross zero. Bisection method will not work!');
     end
@@ -8,12 +8,20 @@ function x = bisection_solver(fun, x_left, x_right)
     b_thresh = 1e-14;
 
     c_old = x_left;
+    
+    guesses_c = [];
+    guesses_fc = [];
 
     for i = 1:max_it
         c = (x_left + x_right) / 2;
         
         delta_x = abs(c - c_old);
         f_c = fun(c);
+
+        if i > 1
+            guesses_c(end+1) = c_old;
+            guesses_fc(end+1) = fun(c_old);
+        end
 
         if delta_x < a_thresh || abs(f_c) < b_thresh
             x = c;
@@ -22,9 +30,9 @@ function x = bisection_solver(fun, x_left, x_right)
         end
         
         if sign(fun(x_left)) == sign(f_c) 
-            x_right = c;
-        else
             x_left = c;
+        else
+            x_right = c;
         end
         
         c_old = c;
--- main.m
+++ main.m
@@ -4,15 +4,19 @@ function main()
     x_left = -15;
     x_right = 10;
     
-    root1 = bisection_solver(@fun, x_left, x_right);
+    [bi_root, bi_c, bi_fc] = bisection_solver(@fun, x_left, x_right);
+    bi_c; %c guesses for bisection
+    bi_fc; %f(c) guesses for bisection
     
-    fprintf('The first root found is: %.14f\n', root1);
+    fprintf('The first root found is: %.14f\n', bi_root);
     
-    %second root
-    x_left = 25;
-    x_right = 40;
-    root2 = bisection_solver(@fun, x_left, x_right);
-    fprintf('The second root found is: %.14f\n', root2);
+    % %second root
+    % x_left = 25;
+    % x_right = 40;
+    % root2 = bisection_solver(@fun, x_left, x_right);
+    % fprintf('The second root found is: %.14f\n', root2);
+
+    x_root = bi_root; % setting true root as root found via bisection, can replace with any other method if desired. 
 
 end
 
