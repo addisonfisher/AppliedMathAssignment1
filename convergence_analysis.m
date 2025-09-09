f4a66bbbeb9f94cf135003cdbab86be287620e42
Addison Fisher <afisher@olin.edu>
2025-09-09 15:16:53 -0400

    generalization (read desc)
    preparing to do generalization, added new file so we can still run main() if necessary or can run convergence_analysis with args

--- convergence_analysis.m
+++ convergence_analysis.m
@@ -1,4 +1,16 @@
-function main()
+%Example template for analysis function
+%INPUTS:
+%solver_flag: an integer from 1-4 indicating which solver to use
+% 1->Bisection 2-> Newton 3->Secant 4->fzero
+%fun: the mathematical function that we are using the
+% solver to compute the root of
+%x_guess0: the initial guess used to compute x_root
+%guess_list1: a list of initial guesses for each trial
+%guess_list2: a second list of initial guesses for each trial
+% if guess_list2 is not needed, then set to zero in input
+%filter_list: a list of constants used to filter the collected data
+function convergence_analysis(solver_flag, fun, ...
+    x_guess0, guess_list1, guess_list2, filter_list)
 
     e_n0 = [];
     e_n1 = [];
@@ -18,12 +30,6 @@ function main()
     end
 
     fprintf('The root found is: %.14f\n', bi_root);
-    
-    % %second root
-    % x_left = 25;
-    % x_right = 40;
-    % root2 = bisection_solver(@fun, x_left, x_right);
-    % fprintf('The second root found is: %.14f\n', root2);
 
     loglog(e_n0,e_n1,'ro','markerfacecolor','r','markersize',1); %loglog sanity check
     hold on;
