b63ec5fc6cf16392738aab21ec3fea0ede1c47b0
lucienjll <llin@olin.edu>
2025-09-09 16:21:11 -0400

    Secant solver updated
    Added lists to secant solver to track guesses

--- secant_method.m
+++ secant_method.m
@@ -1,12 +1,18 @@
 %Secant Solver Problem
 
-function root_finder = secant_method(fun, x0, x1, a_thresh, b_thresh, max_iter)
+function [xn_list, xprev_list, x_count, x_root] = secant_method(fun, x0, x1, a_thresh, b_thresh, max_iter)
 
     %Secant solver takes two initial inputs and updates new guess off of
     %the secant line of the two inputs
+    
+    xn_list = [];
+    xprev_list = [];
+    x_count = [];
 
     x_n = x1;
     x_prev = x0; 
+    
+    count = 1; 
 
     for i = max_iter
         
@@ -29,13 +35,20 @@ function root_finder = secant_method(fun, x0, x1, a_thresh, b_thresh, max_iter)
         end
 
         %Update previous root for secant method 
+        xprev_list = [xprev_list, x_prev];
         x_prev = x_n; 
-
+        
         %Update new root for secant method
         x_n = x_next;
+        xn_list = [xn_list, x_n];
+
+        x_count = [x_count, count];
+        count = count + 1; 
     end 
 
     if i == max_iter
         disp("Root not found")
     end
+
+    x_root = x_n;
 end 
