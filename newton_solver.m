92053ebc410056059987156e6d916b7f41219652
Josephvazhae1 <87318178+Josephvazhae1@users.noreply.github.com>
2025-09-13 14:18:46 -0400

    Newton updated

--- newton_solver.m
+++ newton_solver.m
@@ -1,6 +1,8 @@
+
+
 %Definition of the test function and its derivative
-a_thresh = 1e-14;
-b_thresh = 1e-14;
+% a_thresh = 1e-14;
+% b_thresh = 1e-14;
 
 
 % function root = newton_method(fun, a, b, a_thresh, b_thrsh, max_iter)
@@ -8,14 +10,19 @@ b_thresh = 1e-14;
 %     while (delta_x > a_thresh && )
 % end
 
-test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
-test_derivative01 = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
+% test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
+% test_derivative01 = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
+% 
+% 
+% [root, guesses] = newton_solver(test_func01, test_derivative01, 091, a_thresh, b_thresh, 200); 
 
 
-[root, guesses] = newton_solver(test_func01, test_derivative01, 091, a_thresh, b_thresh, 200);
+function [root, guesses] = newton_solver(fun, dfun, x0) 
 
+    max_iter = 1000;
+    x_thresh = 1e-14;
+    y_thresh = 1e-14;
 
-function [root, guesses] = newton_solver(fun, dfun, x0, x_thresh, y_thresh, max_iter) 
     i = 0;
     guesses = [x0];
     while (i < max_iter)
@@ -29,4 +36,5 @@ function [root, guesses] = newton_solver(fun, dfun, x0, x_thresh, y_thresh, max_
         end
         x0 = x_n;
     end
+    guesses(end + 1) = root;
 end
